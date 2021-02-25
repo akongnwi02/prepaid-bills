448ddf469188100e4b4b070e1eca9d30f7dc02ae
devert <akongnwi02@gmail.com>
2021-02-25 22:35:35 +0100

    Fix problem on heroku deployment

--- config/database.php
+++ config/database.php
@@ -2,7 +2,7 @@
 
 return [
     'redis' => [
-
+        
         'client'  => 'predis',
         'default' => [
             'host'     => env('CACHE_HOST', 'redis'),
@@ -10,18 +10,18 @@ return [
             'port'     => env('CACHE_PORT', 6379),
             'database' => 0,
         ],
-
+        
         'cache'   => [
             'host'     => env('CACHE_HOST', 'redis'),
             'password' => env('CACHE_PASSWORD', null),
             'port'     => env('CACHE_PORT', 6379),
             'database' => env('REDIS_CACHE_DB', 1),
         ],
-
+    
     ],
-
+    
     'connections' => [
-
+        
         'mysql' => [
             'driver' => 'mysql',
             'host' => env('DB_HOST', '127.0.0.1'),
@@ -36,10 +36,23 @@ return [
             'strict' => true,
             'engine' => null,
         ],
+        
+        'pgsql' => [
+            'driver' => 'pgsql',
+            'host' => env('DB_HOST', '127.0.0.1'),
+            'port' => env('DB_PORT', '5432'),
+            'database' => env('DB_DATABASE', 'forge'),
+            'username' => env('DB_USERNAME', 'forge'),
+            'password' => env('DB_PASSWORD', ''),
+            'charset' => 'utf8',
+            'prefix' => '',
+            'schema' => 'public',
+            'sslmode' => 'prefer',
+        ],
     ],
-
-     'migrations' => 'migrations',
-
-    'default' => env('DB_CONNECTION', 'mysql'),
+    
+    'migrations' => 'migrations',
+    
+    'default' => env('DB_CONNECTION', 'pgsql'),
 
 ];
